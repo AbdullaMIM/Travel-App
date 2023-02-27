f851d953b0816876b3e73f535b2add1883924be7
Mohamed Abdulla <abdulhez10@gmail.com>
2023-02-27 12:00:59 +0530

    Update Form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -6,11 +6,11 @@ const ContactForm = () => {
     <div className="form-container">
         <h1>Send a message to us!</h1>
 
-        <form>
-              <input placeholder="Name"/>
-              <input placeholder="Email"/>
-              <input placeholder="Subject"/>
-              <textarea placeholder="Message" rows="4"></textarea>
+        <form method="POST" action="https://getform.io/f/29a3084e-0ccf-44b3-95d1-b50597bc2cc3">
+              <input type="text" placeholder="Name" name="Name" required/>
+              <input type="email" placeholder="Email" name="Email" required/>
+              <input type="text" placeholder="Subject" name="Subject" required/>
+              <textarea placeholder="Message" rows="4" name="Message" required></textarea>
               <button>Send Message</button>
         </form>
     </div>
